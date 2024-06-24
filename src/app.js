1bd6f57b50c2d7cb05916bab61d7e6a000b993d5
meliodas0834 <ks0838066@gmail.com>
2024-06-25 00:57:14 +0530

    setup

--- src/app.js
+++ src/app.js
@@ -14,4 +14,18 @@ app.use(express.urlencoded({extended: true ,limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser());
 
+//routes import 
+import userRouter from "./routes/user.routes.js";
+
+//routes declarartion
+
+app.use("/api/v1/users", userRouter)           // "app.use" is used instead of "app.get" bcz now router ,middleware,controller are in different files
+
+/*   
+  so ,in routes declaration it is prefix of url and it gives control to user.routes.js 
+  and it has another route so it just adds like this :
+  https://localhost:8000/api/v1/users/register
+  and for another route in routes file there will be another new url.
+*/
+
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from './app.js'
 
 dotenv.config({
     path: './env'
@@ -8,10 +9,9 @@ dotenv.config({
 
 
 connectDB()
-
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running on port ${process.env.PORT}`)
+        console.log(`Server is running on port : ${process.env.PORT}`)
     })
 })
 .catch((error) => {
